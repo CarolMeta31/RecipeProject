b1d09f7e8a10441fe8be94f90487926351c09329
Ritumetse <ntshechengpogisho@gmail.com>
2018-09-19 15:37:28 +0200

    navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,9 @@
+import { LogoutPage } from './../pages/logout/logout';
+import { ProfilePage } from './../pages/profile/profile';
+import { FavouritePage } from './../pages/favourite/favourite';
+import { CommunityPage } from './../pages/community/community';
+import { OtherPage } from './../pages/other/other';
+import { TabsPage } from './../pages/tabs/tabs';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -6,11 +12,19 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { SuggestionPage } from '../pages/suggestion/suggestion';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,TabsPage,
+    OtherPage,
+    CommunityPage,
+    SuggestionPage,
+    FavouritePage,
+    ProfilePage,
+    LogoutPage
+
   ],
   imports: [
     BrowserModule,
@@ -19,7 +33,14 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    TabsPage,
+    OtherPage,
+    CommunityPage,
+    SuggestionPage,
+    FavouritePage,
+    ProfilePage,
+    LogoutPage
   ],
   providers: [
     StatusBar,
