bbaf6b270e83616120fb771133091c81a117e135
caro <nxaba31@gmail.com>
2018-09-21 13:11:56 +0200

    changed the auth provider to login.ts and navigation from login and home page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -25,14 +25,58 @@ export class LoginPage {
     console.log('ionViewDidLoad LoginPage');
     this.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
   }
-//navigation to the home page
+//navigation to the home page for guest user
 anonLogin(){
   this.authPro.anonLogin();
   this.navCtrl.push(HomePage)
 }
-//Delegate request to auth provider
-smsLogin(phoneNum: number){
-  this.authPro.smsLogin(phoneNum,this.recaptchaVerifier);
- // this.navCtrl.push(HomePage)
+//connets the phone login and alerts for the OTP
+smsLogin(phoneNumber: number, recaptchaVerifier: firebase.auth.RecaptchaVerifier) {
+  const appVerifier = recaptchaVerifier;
+  const phoneNumberString = "+" + phoneNumber;
+  //Signin with phonenumbers requires BOTH the phone number and a verified captcha
+  firebase.auth().signInWithPhoneNumber(phoneNumberString, appVerifier)
+    .then(confirmationResult => {
+      // SMS sent. Prompt user to type the code from the message, then sign the
+      // user in with confirmationResult.confirm(code).
+      let prompt = this.alertCtrl.create({
+        title: 'Enter the Confirmation code',
+        inputs: [{name: 'confirmationCode', placeholder: 'Confirmation Code'}],
+        buttons: [
+          {
+            text: 'Cancel',
+            handler: data => {
+               //nav back if u cancel OTP 
+            this.navCtrl.pop()
+              console.log('Cancel clicked');
+            }
+          },
+          {
+            text: 'Send',
+            handler: data => {
+              confirmationResult.confirm(data.confirmationCode)
+                .then(function (result) {
+                  // User signed in successfully.
+
+                  console.log(result.user);
+                  //Pop back to the previous page.
+                  this.navCtrl.pop();
+                 // this.navCtrl.push(HomePage)
+                }).catch(function (error) {
+                // User couldn't sign in (bad verification code?)
+                // ...
+              });
+            }
+          }
+        ]
+      });
+      prompt.present();
+      //nav to home page
+      this.navCtrl.push(HomePage);
+    })
+    .catch(function (error) {
+      console.error("SMS not sent", error);
+    });
+
 }
 }
